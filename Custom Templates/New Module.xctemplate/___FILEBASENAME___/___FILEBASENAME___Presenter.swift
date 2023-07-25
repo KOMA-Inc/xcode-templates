b2b473561f84e81155a5aeb3e350b8754d3d9f6a
КолумбиЯ 🦦 <gloomikon@gmail.com>
2023-07-25 21:32:53 +0200

    fix retain cycle

--- Custom Templates/New Module.xctemplate/___FILEBASENAME___/___FILEBASENAME___Presenter.swift
+++ Custom Templates/New Module.xctemplate/___FILEBASENAME___/___FILEBASENAME___Presenter.swift
@@ -6,7 +6,7 @@ class ___VARIABLE_productName:identifier___Presenter {
     // MARK: - Injected properties
 
     private let coordinator: ___VARIABLE_productName:identifier___CoordinatorProtocol
-    private let view: ___VARIABLE_productName:identifier___ViewProtocol
+    private unowned let view: ___VARIABLE_productName:identifier___ViewProtocol
     private let factory: ___VARIABLE_productName:identifier___ViewStateFactory
 
     // MARK: - Private properties
